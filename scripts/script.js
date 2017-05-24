023d1b18467cc124f7b84fd8c05b5415d8a8e496
Rogiel <rogiel2009@gmail.com>
2017-05-24 20:35:15 -0300

    inserido cadastraUsuario e os ids do nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,53 @@
 /*
-File: script.js
-Authors: 
-	Hugo Moraes Dzin, 8532186
-	Matheus Gomes da Silva Horta, 8532321
-	Rogiel dos Santos Silva, 8061793
+ File: script.js
+ Authors:
+ Hugo Moraes Dzin, 8532186
+ Matheus Gomes da Silva Horta, 8532321
+ Rogiel dos Santos Silva, 8061793
  */
-$(document).ready(function(){
+$(document).ready(function() {
 	//Client
-/*	$("input#login").click(function(){
-		$("section#center_page").load("Cliente/funcoesCliente.html");
-	});*/
-	$(document).on("click","a#perfil",function(){
-    	$("section#center_page").load("Cliente/perfil.html");
+	/*	$("input#login").click(function(){
+	 $("section#center_page").load("Cliente/funcoesCliente.html");
+	 });*/
+	$(document).on("click", "a#perfil", function() {
+		$("section#center_page").load("Cliente/perfil.html");
 	});
-	$(document).on("click","a#animais",function(){
-    	$("section#center_page").load("Cliente/situacaoAnimais.html");
+	$(document).on("click", "a#animais", function() {
+		$("section#center_page").load("Cliente/situacaoAnimais.html");
 	});
-	$(document).on("click","a#loja",function(){
-    	$("section#center_page").load("Cliente/loja.html");
+	$(document).on("click", "a#loja", function() {
+		$("section#center_page").load("Cliente/loja.html");
 	});
-	$(document).on("click","a#servicos",function(){
-    	$("section#center_page").load("Cliente/horarioServico.html");
+	$(document).on("click", "a#servicos", function() {
+		$("section#center_page").load("Cliente/horarioServico.html");
 	});
-	$(document).on("click","a#carrinho",function(){
-    	$("section#center_page").load("Cliente/carrinho.html");
+	$(document).on("click", "a#carrinho", function() {
+		$("section#center_page").load("Cliente/carrinho.html");
 	});
 	//Admin
-	
-	$("input#login").click(function(){
+
+	$("input#login").click(function() {
 		$("section#center_page").load("Admin/funcoesAdmin.html");
 	});
-	$(document).on("click","a#perfilAdm",function(){
-    	$("section#center_page").load("Admin/perfil.html");
+	$(document).on("click", "a#perfilAdm", function() {
+		$("section#center_page").load("Admin/perfil.html");
 	});
+	$(document).on("click", "a#cadastrarUsuario", function() {
+		$("section#center_page").load("Admin/cadastrarUsuario.html");
+	});
+
 	/*
-	$(document).on("click","a#animais",function(){
-    	$("section#center_page").load("Cliente/situacaoAnimais.html");
-	});
-	$(document).on("click","a#loja",function(){
-    	$("section#center_page").load("Cliente/loja.html");
-	});
-	$(document).on("click","a#servicos",function(){
-    	$("section#center_page").load("Cliente/horarioServico.html");
-	});
-	$(document).on("click","a#carrinho",function(){
-    	$("section#center_page").load("Cliente/carrinho.html");
-	});*/
-});
+	 $(document).on("click","a#animais",function(){
+	 $("section#center_page").load("Cliente/situacaoAnimais.html");
+	 });
+	 $(document).on("click","a#loja",function(){
+	 $("section#center_page").load("Cliente/loja.html");
+	 });
+	 $(document).on("click","a#servicos",function(){
+	 $("section#center_page").load("Cliente/horarioServico.html");
+	 });
+	 $(document).on("click","a#carrinho",function(){
+	 $("section#center_page").load("Cliente/carrinho.html");
+	 });*/
+}); 
