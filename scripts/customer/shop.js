f962577522ebf6525233880107c82702f3f52ace
Hugo Dzin <humodz@hotmail.com>
2017-06-07 16:04:09 -0300

    add: basic product page

--- scripts/customer/shop.js
+++ scripts/customer/shop.js
@@ -7,19 +7,22 @@
 
 console.log('Executing customer/shop.js');
 
+// TODO busca
+// TODO paginas
 
-
-function _shopArticle(record) {
+// Creates a product <article> element.
+function shopArticle(product) {
 	var html = '';
 
-	html += '<article class="item">';
+	html += '<article class="item" ';
+	html += 'onclick="productPage(' + product.id + ')">';
 
 	html += '<img class="img-reponsive" ';
-	html += 'src="' + record.photo + '" ';
-	html += 'alt="' + record.name  + '" />';
+	html += 'src="' + product.photo + '" ';
+	html += 'alt="' + product.name  + '" />';
 
-	html += '<p>'    + record.name  + '</p>';
-	html += '<p>R$ ' + record.price + '</p>';
+	html += '<p>'    + product.name  + '</p>';
+	html += '<p>R$ ' + product.price + '</p>';
 
 	html += '</article>';
 
@@ -28,13 +31,12 @@ function _shopArticle(record) {
 
 
 
+// Reads all products from the database and loads them on the page
 function customerShop() {
 	dbReadAllRecords('products', function(result) {
-		if(result.success)
-		{
+		if(result.success) {
 			result.data.forEach(function (product) {
-				console.log(product);
-				$('#cShopProducts').append(_shopArticle(product));
+				$('#cShopProducts').append(shopArticle(product));
 			});
 		}
 	});
@@ -42,6 +44,45 @@ function customerShop() {
 
 
 
-function loadShopProducts() {
+// Called when the user clicks on a product. Loads the product's page
+function productPage(product_id) {
+	loadPage('Cliente/compraProduto.html', function() {
+		dbReadRecord(product_id, 'products', function(result) {
+			if(result.success) {
+				var product = result.data;
 
+				$('#cProductPhoto').attr('src', product.photo);
+
+				$('#cProductTitle').html(product.name);
+				$('#cProductDescription').html(product.description);
+				$('#cProductPrice').html(product.price);
+				$('#cProductStock').html(product.stock);
+
+				$('#cPurchaseAmount').val(0);
+				$('#cPurchaseAmount').change(function(event) {
+					var target = $('#cPurchaseAmount');
+
+					if(target.val() < 0) {
+						target.val(0);
+					}
+					else if(target.val() > product.stock) {
+						target.val(product.stock);
+					}
+				});
+
+				$('#cAddToCart').click(function() {
+					var amount = parseInt($('#cPurchaseAmount').val(), 10);
+					goToCart(product.id, amount);
+				});
+			}
+		});
+	});
+}
+
+
+
+// Adds the product and its amount to the user's shopping cart,
+// then loads the shopping cart page
+function goToCart(product_id, amount) {
+	console.log(product_id, amount);
 }
